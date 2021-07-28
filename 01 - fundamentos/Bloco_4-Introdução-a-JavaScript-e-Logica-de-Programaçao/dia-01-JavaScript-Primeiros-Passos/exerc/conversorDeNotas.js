8ac5d9c05775cbaa8c6ee82d52cebb003772f209
claudiocassimiro <days_surfing@live.com>
2021-07-28 20:35:27 -0300

    Implementa um conversor de notas

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-01-JavaScript-Primeiros-Passos/exerc/conversorDeNotas.js
@@ -0,0 +1,23 @@
+let porcentagem = 100;
+
+if (porcentagem < 0 || porcentagem > 100) {
+    console.log('Error');
+}
+else if (porcentagem >= 90) {
+    console.log('Nota A');
+}
+else if (porcentagem >= 80) {
+    console.log('Nota B');
+}
+else if (porcentagem >= 70) {
+    console.log('Nota C');
+}
+else if (porcentagem >= 60) {
+    console.log('Nota D');
+}
+else if (porcentagem >= 50) {
+    console.log('Nota E');
+}
+else {
+    console.log('Nota F');
+}
