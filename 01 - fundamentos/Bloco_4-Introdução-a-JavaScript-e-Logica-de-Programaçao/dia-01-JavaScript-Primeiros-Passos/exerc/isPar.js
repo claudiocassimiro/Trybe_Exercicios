50f51755eab0f3fd6e0154e976e94ef6768c78a6
claudiocassimiro <days_surfing@live.com>
2021-07-28 20:41:29 -0300

    Implementa programa que verifica se tem valor par

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-01-JavaScript-Primeiros-Passos/exerc/isPar.js
@@ -0,0 +1,12 @@
+// Exercicio 8
+
+let num1 = 7;
+let num2 = 9;
+let num3 = 13;
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0 ) {
+    console.log(true);
+} 
+else {
+    console.log(false);
+}
