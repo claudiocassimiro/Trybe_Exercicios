78ec0445339e7563b266ffb60a22a9320910a151
claudiocassimiro <days_surfing@live.com>
2021-07-28 20:43:36 -0300

    Implementa programa que verifica se existe números impar

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-01-JavaScript-Primeiros-Passos/exerc/isImpar.js
@@ -0,0 +1,12 @@
+// Exercicio 9
+
+let num1 = 0;
+let num2 = 0;
+let num3 = 0;
+
+if (num1 % 2 === 1 || num2 % 2 === 1 || num3 % 2 === 1 ) {
+    console.log(true);
+} 
+else {
+    console.log(false);
+}
