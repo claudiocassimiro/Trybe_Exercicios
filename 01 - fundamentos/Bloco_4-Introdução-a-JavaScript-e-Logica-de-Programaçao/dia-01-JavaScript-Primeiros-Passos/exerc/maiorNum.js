c08cec23054fb5591a1bd4f74ed4c490b05a99b8
claudiocassimiro <days_surfing@live.com>
2021-07-28 19:25:46 -0300

    Alteração no nome da pasta de exercicios

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-01-JavaScript-Primeiros-Passos/exerc/maiorNum.js
@@ -0,0 +1,12 @@
+// Exercicio 2
+let num1 = 11;
+let num2 = 10;
+
+
+
+if (num1 > num2) {
+    console.log(`O maior entre os valores é ${num1}`);
+} 
+else {
+    console.log(`O maior entre os valores é ${num2}`);
+}
---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-01-JavaScript-Primeiros-Passos/exerc/positiveNegativeOrZero.js
@@ -0,0 +1,15 @@
+// Exercicio 3
+let num = "xablau";
+
+if (num === 0) {
+    console.log('Zero');
+}
+else if (num < 0) {
+    console.log('Negative');
+}
+else if (num > 0) {
+    console.log('Positive');
+}
+else {
+    console.log('Digite um valor valido!');
+}
