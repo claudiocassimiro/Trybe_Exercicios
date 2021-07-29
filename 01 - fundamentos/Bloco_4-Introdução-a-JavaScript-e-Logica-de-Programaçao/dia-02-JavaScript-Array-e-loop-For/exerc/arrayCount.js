037394f13a4b54a1c3e2dbc4ed96b9d275bcc6ea
claudiocassimiro <days_surfing@live.com>
2021-07-29 19:36:38 -0300

    Implementa uma melhor solução para o problema do exercicio 8

--- 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-02-JavaScript-Array-e-loop-For/exerc/arrayCount.js
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-02-JavaScript-Array-e-loop-For/exerc/arrayCount.js
@@ -1,9 +1,6 @@
 // Exercicio 8
+
 let vet = []
 for (let index = 1; index <= 25; index += 1) {
     vet[index] = index;
-}
-
-for (let index2 = 1; index2 < vet.length; index2 += 1) {
-    console.log(vet[index2]);
 }
---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-02-JavaScript-Array-e-loop-For/exerc/divisionArray.js
@@ -0,0 +1,17 @@
+// Exercicio 9
+
+let vet = []
+
+for (let index = 1; index <= 25; index += 1) {
+    vet[index] = index;
+    console.log(vet[index]);
+}
+
+// let division = []
+
+// for (let index2 = 1; index2 < vet.length; index2 += 1) {
+//     division[index2] = vet[index2] / 2;
+// }
+
+// console.log(division);
+
