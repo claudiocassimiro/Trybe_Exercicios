1ce712ee5db57adf26edb56654b6037107778efc
claudiocassimiro <days_surfing@live.com>
2021-07-30 18:28:02 -0300

    Renamed file

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterTriangleRectangleInverted.js
@@ -0,0 +1,23 @@
+// Exercicio 3
+
+let n = 5;
+
+for (let i = 1; i <= n; i += 1) {
+    switch (i) {
+        case 1:
+            console.log('    *');
+            break;
+        case 2:
+            console.log('   **');
+            break;
+        case 3:
+            console.log('  ***');
+            break;
+        case 4:
+            console.log(' ****');
+            break;
+        case 5:
+            console.log('*****');
+            break;
+    }
+}
