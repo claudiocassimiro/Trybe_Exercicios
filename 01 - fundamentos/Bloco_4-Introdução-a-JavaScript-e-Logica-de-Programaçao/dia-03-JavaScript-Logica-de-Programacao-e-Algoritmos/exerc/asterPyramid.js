bfc085f45f837f39c577d1ab1acd23fbf1b32ce1
claudiocassimiro <days_surfing@live.com>
2021-07-30 18:32:04 -0300

    Implementa solução para o exercicio 4

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterPyramid.js
@@ -0,0 +1,17 @@
+// Exercicicio 4
+
+let n = 3;
+
+for (let i = 1; i <= n; i += 1) {
+    switch (i) {
+        case 1:
+            console.log('  *  ');
+            break;
+        case 2:
+            console.log(' *** ');
+            break;
+        case 3:
+            console.log('*****');
+            break;
+    }
+}
