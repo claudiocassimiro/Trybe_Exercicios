f40dad31db0557fcfb7469700d944e5761cb600f
claudiocassimiro <days_surfing@live.com>
2021-07-30 18:49:48 -0300

    Implementa solução para o exercicio bonus 5

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterPyramidVazed.js
@@ -0,0 +1,29 @@
+// Exercicio bonus 5
+
+let n = 7;
+
+for (let i = 1; i <= n; i += 1) {
+    switch (i) {
+        case 1:
+            console.log('   *   ');
+            break;
+        case 2:
+            console.log('');
+            break;
+        case 3:
+            console.log('  * *  ');
+            break;
+        case 4:
+            console.log('');
+            break;
+        case 5:
+            console.log(' *   * ');
+            break;
+        case 6:
+            console.log('');
+            break;
+        case 7:
+            console.log('*******');
+            break;
+    }
+}
