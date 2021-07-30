b305b18516d51ce2c3cb33c4eefac35ac8f57b2c
claudiocassimiro <days_surfing@live.com>
2021-07-30 11:31:22 -0300

    Implementa uma sulução melhor que a anterior para o exercicio bonus 1

--- 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-02-JavaScript-Array-e-loop-For/exerc/bubbleSortCrescente.js
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-02-JavaScript-Array-e-loop-For/exerc/bubbleSortCrescente.js
@@ -4,11 +4,11 @@ let array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 let aux = 0;
 
 for (let index = 1; index < array.length; index += 1) {
-    for (let secondIndex = 0; secondIndex < array.length - index; secondIndex += 1) {
-        if (array[secondIndex] > array[secondIndex + 1]) {
-            aux = array[secondIndex];
-            array[secondIndex] = array[secondIndex + 1];
-            array[secondIndex + 1] = aux;
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+        if (array[index] < array[secondIndex]) {
+            aux = array[index];
+            array[index] = array[secondIndex];
+            array[secondIndex] = aux;
         }
     }
 }
