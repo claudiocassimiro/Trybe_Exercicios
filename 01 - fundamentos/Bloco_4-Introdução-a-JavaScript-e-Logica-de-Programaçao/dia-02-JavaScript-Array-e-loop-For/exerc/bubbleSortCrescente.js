22ba2464b61bbe1244ed3118e6ccdd1f9d5e424e
claudiocassimiro <days_surfing@live.com>
2021-07-30 11:36:22 -0300

    Muda o nome do array e seus valores

--- 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-02-JavaScript-Array-e-loop-For/exerc/bubbleSortCrescente.js
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-02-JavaScript-Array-e-loop-For/exerc/bubbleSortCrescente.js
@@ -1,16 +1,16 @@
 // Exercicio bonus 1
 
-let array = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let aux = 0;
 
-for (let index = 1; index < array.length; index += 1) {
+for (let index = 1; index < numbers.length; index += 1) {
     for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-        if (array[index] < array[secondIndex]) {
-            aux = array[index];
-            array[index] = array[secondIndex];
-            array[secondIndex] = aux;
+        if (numbers[index] < numbers[secondIndex]) {
+            aux = numbers[index];
+            numbers[index] = numbers[secondIndex];
+            numbers[secondIndex] = aux;
         }
     }
 }
 
-console.log(array);
+console.log(numbers);
