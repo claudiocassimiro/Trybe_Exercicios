3e4390d2e552ed50e786f65dc1ddc8baae530319
claudiocassimiro <days_surfing@live.com>
2021-07-30 23:10:48 -0300

    Implementa uma nova solução

--- 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterTriangleRectangleInverted.js
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterTriangleRectangleInverted.js
@@ -1,23 +1,82 @@
 // Exercicio 3
 
-let n = 5;
+// variaveis
 
-for (let i = 1; i <= n; i += 1) {
-    switch (i) {
-        case 1:
-            console.log('    *');
-            break;
-        case 2:
-            console.log('   **');
-            break;
-        case 3:
-            console.log('  ***');
-            break;
-        case 4:
-            console.log(' ****');
-            break;
-        case 5:
-            console.log('*****');
-            break;
+// Preciso de uma variavel para ser o valor inicial n
+// Preciso de uma variavel para ser o simbolo
+// preciso de uma variavel para ser o space
+// preciso de uma variavel para marcar a posição que vai receber n
+
+// estruturas
+
+// percorrer as linhas de 0 até n - 1
+    // percorrer as colunas de 0 até n  
+        // caso colunas < position
+            // concatena mais um espaço a space
+        // senão
+            // concatena um symbol a variavel space
+    // ^
+// função de output
+// zerá a variavel space
+// subtrai um valor de position
+
+let n = 20;
+let symbol = '*';
+let space = ' ';
+let position = n;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    for (let colunmIndex = 0; colunmIndex <= n; colunmIndex += 1) {
+        if (colunmIndex < position) {
+            space += ' ';
+        }
+        else {
+            space += symbol;
+        }
     }
+    console.log(space)
+    space = ' ';
+    position -= 1;
 }
+
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+// let inputPosition = n;
+
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//   for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+//     if (columnIndex < inputPosition) {
+//       inputLine = inputLine + ' ';
+//     } else {
+//       inputLine = inputLine + symbol;
+//     }
+//   }
+//   console.log(inputLine);
+//   inputLine = '';
+//   inputPosition -= 1;
+// };
+
+// Resolução antiga
+
+// let n = 5;
+
+// for (let i = 1; i <= n; i += 1) {
+//     switch (i) {
+//         case 1:
+//             console.log('    *');
+//             break;
+//         case 2:
+//             console.log('   **');
+//             break;
+//         case 3:
+//             console.log('  ***');
+//             break;
+//         case 4:
+//             console.log(' ****');
+//             break;
+//         case 5:
+//             console.log('*****');
+//             break;
+//     }
+// }
