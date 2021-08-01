9954a6cccce02fa8d1cf3ac1c8439be6d3ac2797
claudiocassimiro <days_surfing@live.com>
2021-08-01 12:50:10 -0300

    Nova solução para o exercicio bonus 5

--- 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterPyramidVazed.js
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterPyramidVazed.js
@@ -1,29 +1,53 @@
 // Exercicio bonus 5
 
 let n = 7;
+let symbol = '*';
+let space = '';
+let midOfPyramid = (n + 1) / 2;
 
-for (let i = 1; i <= n; i += 1) {
-    switch (i) {
-        case 1:
-            console.log('   *   ');
-            break;
-        case 2:
-            console.log('');
-            break;
-        case 3:
-            console.log('  * *  ');
-            break;
-        case 4:
-            console.log('');
-            break;
-        case 5:
-            console.log(' *   * ');
-            break;
-        case 6:
-            console.log('');
-            break;
-        case 7:
-            console.log('*******');
-            break;
+let controlLeft = midOfPyramid;
+let controlRight = midOfPyramid;
+
+for (let lineIndex = 1; lineIndex <= midOfPyramid; lineIndex +=1) {
+    let space = '';
+    for (let colunmIndex = 1; colunmIndex <= n; colunmIndex += 1) {
+        if (colunmIndex == controlLeft || colunmIndex == controlRight || lineIndex == midOfPyramid) {
+            space += symbol;
+        }
+        else {
+            space += ' ';
+        }
     }
+    controlLeft -= 1;
+    controlRight += 1;
+    console.log(space);
 }
+// Resolução antiga
+
+// let n = 7;
+
+// for (let i = 1; i <= n; i += 1) {
+//     switch (i) {
+//         case 1:
+//             console.log('   *   ');
+//             break;
+//         case 2:
+//             console.log('');
+//             break;
+//         case 3:
+//             console.log('  * *  ');
+//             break;
+//         case 4:
+//             console.log('');
+//             break;
+//         case 5:
+//             console.log(' *   * ');
+//             break;
+//         case 6:
+//             console.log('');
+//             break;
+//         case 7:
+//             console.log('*******');
+//             break;
+//     }
+// }
