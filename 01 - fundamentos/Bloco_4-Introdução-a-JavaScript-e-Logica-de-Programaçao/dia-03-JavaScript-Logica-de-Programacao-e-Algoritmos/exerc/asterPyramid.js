f8eed3e6a5900fe2d0ebf66fec5db5dd61e8c369
claudiocassimiro <days_surfing@live.com>
2021-08-01 11:33:44 -0300

    Nova solução para o exercicio 4

--- 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterPyramid.js
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-03-JavaScript-Logica-de-Programacao-e-Algoritmos/exerc/asterPyramid.js
@@ -1,23 +1,49 @@
 // Exercicicio 4
 
-let n = 5;
+let n = 9;
+let symbol = '*';
+let space = '';
+let midOfPyramid = (n + 1) / 2;
 
-for (let i = 1; i <= n; i += 1) {
-    switch (i) {
-        case 1:
-            console.log('  *  ');
-            break;
-        case 2:
-            console.log('');
-            break;
-        case 3:
-            console.log(' *** ');
-            break;
-        case 4:
-            console.log('');
-            break;
-        case 5:
-            console.log('*****');
-            break;
+let controlLeft = midOfPyramid;
+let controlRight = midOfPyramid;
+
+
+for (let lineIndex = 0; lineIndex <= midOfPyramid; lineIndex += 1) {
+    for (let colunmIndex = 0; colunmIndex <= n; colunmIndex += 1) {
+        if (colunmIndex > controlLeft && colunmIndex < controlRight) {
+            space = space + symbol;
+        } 
+        else {
+            space = space + ' ';
+        }
     }
+    console.log(space)
+    space = '';
+    controlRight += 1;
+    controlLeft -= 1;  
 }
+
+// Resolução antiga
+
+// let n = 5;
+
+// for (let i = 1; i <= n; i += 1) {
+//     switch (i) {
+//         case 1:
+//             console.log('  *  ');
+//             break;
+//         case 2:
+//             console.log('');
+//             break;
+//         case 3:
+//             console.log(' *** ');
+//             break;
+//         case 4:
+//             console.log('');
+//             break;
+//         case 5:
+//             console.log('*****');
+//             break;
+//     }
+// }
