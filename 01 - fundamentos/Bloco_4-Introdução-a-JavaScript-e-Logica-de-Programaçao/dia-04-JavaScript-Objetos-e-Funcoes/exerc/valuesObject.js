1c11043363f899d4f4abc449997a89e436c3b2a2
claudiocassimiro <days_surfing@live.com>
2021-08-02 20:37:46 -0300

    Implementa solução para o exercicio 4

---
+++ 01 - fundamentos/Bloco_4-Introdução-a-JavaScript-e-Logica-de-Programaçao/dia-04-JavaScript-Objetos-e-Funcoes/exerc/valuesObject.js
@@ -0,0 +1,13 @@
+// Exercicio 4
+
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  };
+
+  info.recorrente = 'Sim';
+
+  for (let key in info) {
+      console.log(info[key]);
+  }
