f73c40751ec7b49bb2a6fdc410f3da9f6d1de300
claudiocassimiro <days_surfing@live.com>
2021-08-12 10:04:42 -0300

    Exercicio 1 completo

--- 01 - fundamentos/Bloco_5-JavaScript-Dom-Eventos-e-WebStorage/dia_03-Eventos/exerc/script.js
+++ 01 - fundamentos/Bloco_5-JavaScript-Dom-Eventos-e-WebStorage/dia_03-Eventos/exerc/script.js
@@ -8,9 +8,22 @@ function createDaysOfTheWeek() {
       dayListItem.innerHTML = days;
   
       weekDaysList.appendChild(dayListItem);
-    };
-  };
+    }
+}
   
-  createDaysOfTheWeek();
-  
-  // Escreva seu código abaixo.
+createDaysOfTheWeek();
+
+function createDaysOfThemMonth() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const calendarList = document.querySelector('#days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const dayNumlistItem = document.createElement('li');
+        dayNumlistItem.innerHTML = dezDaysList[index];
+        dayNumlistItem.className = 'day';
+
+        calendarList.appendChild(dayNumlistItem);
+    }
+}
+
+createDaysOfThemMonth();
