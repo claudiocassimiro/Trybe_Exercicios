0e393ac4e5dcd36b049b437b1a4ad1307fde7779
claudiocassimiro <days_surfing@live.com>
2021-08-31 18:55:18 -0300

    Testes para o exercicio 2 completos

---
+++ 01 - fundamentos/Bloco_7-JavaScript-ES6/Testes-Unitarios-em-JavaScript/exerc1/test-unitario-2.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let index = 0; index < arr.length; index += 1) {
+    if (item !== arr[index]) {
+      newArr.push(arr[index]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+// Verifique se o array passado por parâmetro não sofreu alterações
+const list = [5, 7, 9, 3];
+myRemove(list, 5);
+assert.deepStrictEqual(list, [5, 7, 9, 3]);
+
+// Verifique se a chamada myRemove([1, 2, 3, 4], 5) retorna o array esperado
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
