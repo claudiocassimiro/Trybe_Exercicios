f93d06b0695560d84617b998ce2dd5715623a42c
claudiocassimiro <days_surfing@live.com>
2021-08-31 07:52:33 -0300

    Primeiro e segundo exercicio da lista de exercicios 2 completos

---
+++ 01 - fundamentos/Bloco_7-JavaScript-ES6/JavaScriptES6-Objects/exerc2/script.js
@@ -0,0 +1,35 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+};
+  
+const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+};
+  
+const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+};
+
+// Crie uma função para adicionar o turno da manhã na lesson2.
+const generateKeys = (obj, key, value) => {obj[key] = value};
+
+generateKeys(lesson2, 'turno', 'manhã');
+
+// Crie uma função para listar as keys de um objeto.
+const listKeys = (obj) => {
+    const keys = Object.keys(obj);
+    
+    for (const key of keys) {
+        console.log(key);
+    }
+}
+
+listKeys(lesson2);
