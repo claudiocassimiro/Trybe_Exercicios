a13cb14ad468b8e9a910eb88db06158725d68259
claudiocassimiro <days_surfing@live.com>
2021-09-17 10:39:04 -0300

    Finaliza o Exercicio 3

--- 01 - fundamentos/Bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exerc/promise.js
+++ 01 - fundamentos/Bloco-9-javascript-assincrono-e-promises/dia-2-javascript-promises/exerc/promise.js
@@ -3,14 +3,14 @@ const sumNumbers = (numbers) => numbers.reduce((acc, num) => acc + num, 0);
 const numbersForDivision = [2, 3, 5, 10];
 
 const createArrayWithDivision = (num) => {
-    const newArr = numbersForDivision.map((number) => num / number);
+    const newArr = numbersForDivision.map((number) => parseInt(num / number));
     return newArr;
 };
 
 const promise = new Promise((resolve, reject) => {
     const randomNumbers = [];
     for (let i = 0; i < 10; i += 1) {
-        const randomNum = parseInt(Math.random(1) * 50);
+        const randomNum = parseInt(Math.random(1) * 51);
         randomNumbers.push(randomNum);
     }
     let sum = sumNumbers(randomNumbers);
@@ -18,8 +18,16 @@ const promise = new Promise((resolve, reject) => {
     if (sum < 8000) {
         resolve(createArrayWithDivision(sum));
     } else {
-        reject('Error')
+        reject('console.log , a frase "É mais de oito mil! Essa promise deve estar quebrada!"');
     }
 })
-.then((result) => console.log(result))
+.then((result) => {
+    console.log(result);
+    const newArr = result.map((number) => number);
+    return newArr;
+})
+.then((resulted) => {
+    const newArrSumed = resulted.reduce((acc, num) => acc + num, 0);
+    console.log(parseInt(newArrSumed));
+})
 .catch((error) => console.log(error));
