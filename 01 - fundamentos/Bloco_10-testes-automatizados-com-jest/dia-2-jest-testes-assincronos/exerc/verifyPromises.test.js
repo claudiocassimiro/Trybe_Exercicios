79a4f7a991b521f46b99c0af001655f10bc72260
claudiocassimiro <days_surfing@live.com>
2021-09-22 21:45:14 -0300

    Requisito 3 completo

--- 01 - fundamentos/Bloco_10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exerc/verifyPromises.test.js
+++ 01 - fundamentos/Bloco_10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exerc/verifyPromises.test.js
@@ -1,21 +1,44 @@
 const { findUserById, getUserName, users } = require('./verifyPromises');
 
-describe('testa o caso de sucesso e falha da requisição', () => {
 
-    it('em caso de sucesso', () => {
-        expect.assertions(1);
+// TESTE UTILIZANDO ASYNC E AWAIT...
+// describe('testa o caso de sucesso e falha da requisição', () => {
 
-        getUserName(1).then((name) => {
-            expect(name).toEqual(users[0].name);
-        });
-    });
+//     it('em caso de sucesso', async() => {
+//         expect.assertions(1);
 
-    it('em caso de falha', () => {
-        expect.assertions(1);
+//         const name = await getUserName(2);
+//         expect(name).toEqual(users[1].name);
+//     });
 
-        getUserName(3).catch((error) => {
-            expect(error).toBe(error);
-        })
-    });
-});
+//     it('em caso de falha', async() => {
+//         expect.assertions(1);
+
+//         try {
+//             const name = await getUserName(3);
+//         } catch(error) {
+//             expect(error).toBe(error);
+//         }
+//     });
+// })
+
+// TESTE UTILIZANDO .THEN E .CATCH...
+// describe('testa o caso de sucesso e falha da requisição', () => {
+
+//     it('em caso de sucesso', () => {
+//         expect.assertions(1);
+
+//         getUserName(1).then((name) => {
+//             expect(name).toEqual(users[0].name);
+//         });
+//     });
+
+//     it('em caso de falha', () => {
+//         expect.assertions(1);
+
+//         getUserName(3).catch((error) => {
+//             expect(error).toBe(error);
+//         })
+//     });
+// });
 
