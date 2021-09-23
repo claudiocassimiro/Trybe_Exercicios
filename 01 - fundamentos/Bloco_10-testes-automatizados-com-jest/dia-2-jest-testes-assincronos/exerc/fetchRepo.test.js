ad06e7b3b7b8de699bc952ca0d87fba109936b67
claudiocassimiro <days_surfing@live.com>
2021-09-23 10:21:33 -0300

    adiciona testes dos exercicios do dia 2

--- 01 - fundamentos/Bloco_10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exerc/fetchRepo.test.js
+++ 01 - fundamentos/Bloco_10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exerc/fetchRepo.test.js
@@ -1,10 +1,10 @@
 const getRepos = require('./fetchRepo');
 
-test('Dada uma url o retorno é uma lista com os repo?', async() => {
+test('Dada uma url o retorno é uma lista contendo os repos?', async() => {
     const url = 'https://api.github.com/orgs/tryber/repos';
     const resp = await getRepos(url);
-    
-    expect.assetion(2);
+
+    expect.assertions(2);
     expect(resp).toContain('sd-01-week4-5-project-todo-list');
     expect(resp).toContain('sd-01-week4-5-project-meme-generator');
 });
