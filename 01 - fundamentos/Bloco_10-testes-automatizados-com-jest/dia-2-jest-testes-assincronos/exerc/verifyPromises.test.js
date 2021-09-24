9a50392165ee4239a6808da07e0baa2b6af6ba5c
claudiocassimiro <days_surfing@live.com>
2021-09-24 08:54:48 -0300

    Refatora o veriFyPromises.test.js

--- 01 - fundamentos/Bloco_10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exerc/verifyPromises.test.js
+++ 01 - fundamentos/Bloco_10-testes-automatizados-com-jest/dia-2-jest-testes-assincronos/exerc/verifyPromises.test.js
@@ -2,25 +2,25 @@ const { findUserById, getUserName, users } = require('./verifyPromises');
 
 
 // TESTE UTILIZANDO ASYNC E AWAIT...
-// describe('testa o caso de sucesso e falha da requisição', () => {
+describe('testa o caso de sucesso e falha da requisição', () => {
 
-//     it('em caso de sucesso', async() => {
-//         expect.assertions(1);
+    it('em caso de sucesso', async() => {
+        expect.assertions(1);
 
-//         const name = await getUserName(2);
-//         expect(name).toEqual(users[1].name);
-//     });
+        const name = await getUserName(2);
+        expect(name).toEqual(users[1].name);
+    });
 
-//     it('em caso de falha', async() => {
-//         expect.assertions(1);
+    it('em caso de falha', async() => {
+        expect.assertions(1);
 
-//         try {
-//             const name = await getUserName(3);
-//         } catch(error) {
-//             expect(error).toBe(error);
-//         }
-//     });
-// })
+        try {
+            const name = await getUserName(3);
+        } catch(error) {
+            expect(error).toBe(error);
+        }
+    });
+})
 
 // TESTE UTILIZANDO .THEN E .CATCH...
 // describe('testa o caso de sucesso e falha da requisição', () => {
